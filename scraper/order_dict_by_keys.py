22868e9a7b2ce3e1bef178555190c339022fbe5d
badal <develop.badal@gmail.com>
2023-03-18 22:03:44 +0530

    feat: ordered dict

--- scraper/order_dict_by_keys.py
+++ scraper/order_dict_by_keys.py
@@ -14,11 +14,11 @@ with open(f'../src/kanda/{kanda}/chapters.json') as json_file:
     for index, x in enumerate(json_data):
         with open(f'../src/kanda/{kanda}/prose/chapter/{x["sarga"]}.json') as chapter_json_fle:
             chapter_json_list = json.load(chapter_json_fle)
-            # print(chapter_json_list)
-            d = chapter_json_list      # dictionary containing key-value pairs that are to be ordered
-            l = ordered_list  # list of keys that represent the order for the dictionary
-            # retrieve the values in order and build a list of ordered key-value pairs
-            ordered_dict_items = [(k, d[k]) for k in l]
+            # https://stackoverflow.com/a/70985799/7679903
+            ordered_dict = {}
+            for item in ordered_list:
+                ordered_dict[item] = chapter_json_list[item]
+
             # print(ordered_dict_items)
-            dumpAsJson(ordered_dict_items,
+            dumpAsJson(ordered_dict,
                        f'../src/kanda/{kanda}/prose/chapter/{x["sarga"]}.json')
